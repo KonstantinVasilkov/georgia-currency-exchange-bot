b696051c0f77b69239283389c847d840adce0752
Konstantin <nordisland65@gmail.com>
2025-05-04 22:58:11 +0400

    Add scheduler implementation with tests and map sync support

--- src/db/migrations/env.py
+++ src/db/migrations/env.py
@@ -23,6 +23,13 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 target_metadata = SQLModel.metadata
+target_metadata.naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- src/db/models/__init__.py
+++ src/db/models/__init__.py
@@ -3,4 +3,6 @@ from src.db.models.office import Office
 from src.db.models.organization import Organization
 from src.db.models.rate import Rate
 
-__all__ = ["BaseModel", "Office", "Organization", "Rate"]
+__all__ = ["BaseModel", "Office", "Organization", "Rate", "OfficeSchedule"]
+
+from src.db.models.schedule import OfficeSchedule
